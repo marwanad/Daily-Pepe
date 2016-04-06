acb0e7b94313e44d8e73a277085e243b9d440ec2
marwanad <mrwnad@gmail.com>
2016-04-06 01:24:15 -0400

    logging

--- app/bot/bot.py
+++ app/bot/bot.py
@@ -8,10 +8,12 @@ from ..setup import kik, bot_config
 
 @main.route('/receive', methods=['POST'])
 def incoming():
+    print("incoming")
     if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
         return Response(status=403)
 
     messages = messages_from_json(request.json['messages'])
+    print(messages)
     for message in messages:
         if isinstance(message, TextMessage):
             kik.send_messages([
